3a3b17654ae9d9a0fe320fffca3b8cd1838c3797
John Swinbank <john@swinbank.org>
2015-11-26 13:41:25 -0500

    Add support for OSX Notification Center (sic).

--- fourpiskytools/notify.py
+++ fourpiskytools/notify.py
@@ -22,13 +22,39 @@ try:
             n.show()
 
 except ImportError:
-    import logging
+    try:
+        from Foundation import NSBundle
+        from objc import lookUpClass
 
-    class Notifier(object):
-        """
-        Writes a message to the log.
-        """
-        def send_notification(self, title, text):
-            logger = logging.getLogger("notifier")
-            logger.info(title)
-            logger.info(text)
+        class Notifier(object):
+            """
+            On OSX displays a message in the Notification Center.
+            """
+            def __init__(self):
+                # It's only possible to send notifications if we have a bundle
+                # identifier set. This happens by default if using Python
+                # installed as a Framework (e.g. the system Python), but isn't
+                # set in a virtualenv.
+                NSBundle.mainBundle().infoDictionary().setdefault(
+                    "CFBundleIdentifier", "org.4pisky.tools")
+                self.NSUserNotification = lookUpClass("NSUserNotification")
+                NCenter = lookUpClass("NSUserNotificationCenter")
+                self.center = NCenter.defaultUserNotificationCenter()
+
+            def send_notification(self, title, text, icon="ignored"):
+                n = self.NSUserNotification.alloc().init()
+                n.setTitle_(title)
+                n.setInformativeText_(text)
+                self.center.scheduleNotification_(n)
+
+    except ImportError:
+        import logging
+
+        class Notifier(object):
+            """
+            Writes a message to the log.
+            """
+            def send_notification(self, title, text):
+                logger = logging.getLogger("notifier")
+                logger.info(title)
+                logger.info(text)
