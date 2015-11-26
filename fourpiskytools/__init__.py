b867625834b41aeeafdbb4a9a348480754abb565
John Swinbank <john@swinbank.org>
2015-11-26 13:40:29 -0500

    Auto-select notifier based on available modules.

--- fourpiskytools/__init__.py
+++ fourpiskytools/__init__.py
@@ -3,18 +3,4 @@ from __future__ import absolute_import
 import fourpiskytools.comet
 import fourpiskytools.identity
 import fourpiskytools.voevent
-#import fourpiskytools.notify
-
-import logging
-
-class SimpleNotifier():
-    """
-    Drop in replacement for fourpiskytools.notify.Notifier, works without PGI
-
-    (Usable without any extra faff on Mac OSX, basically.)
-
-    """
-    def send_notification(self, title, text):
-        logger = logging.getLogger("script2")
-        logger.info(title)
-        logger.info(text)
+import fourpiskytools.notify
--- fourpiskytools/notify.py
+++ fourpiskytools/notify.py
@@ -1,19 +1,34 @@
-from pgi.repository import Notify
-from pgi.repository import GObject
+"""
+Choose an appropriate method of providing notifications
+based on the available modules.
+"""
 
-# Inspired by http://askubuntu.com/questions/108764,
-# with several tweaks and fixes applied.
+try:
+    # Inspired by http://askubuntu.com/questions/108764,
+    # with several tweaks and fixes applied.
+    from pgi.repository import Notify
+    from pgi.repository import GObject
 
-class Notifier(GObject.Object):
-    """
-    On *buntu systems, displays a message as a system notification.
-    """
-    def __init__(self):
+    class Notifier(GObject.Object):
+        """
+        On *buntu systems, displays a message as a system notification.
+        """
+        def __init__(self):
+            super(Notifier, self).__init__()
+            Notify.init("VOEvent Notifications")
 
-        super(Notifier, self).__init__()
-        Notify.init("VOEvent Notifications")
+        def send_notification(self, title, text, icon="mail-unread"):
+            n = Notify.Notification.new(title, text, icon=icon)
+            n.show()
 
-    def send_notification(self, title, text, icon="mail-unread"):
-        n = Notify.Notification.new(title, text, icon=icon)
-        n.show()
+except ImportError:
+    import logging
 
+    class Notifier(object):
+        """
+        Writes a message to the log.
+        """
+        def send_notification(self, title, text):
+            logger = logging.getLogger("notifier")
+            logger.info(title)
+            logger.info(text)
