0666d8375fab401a116a65adb8de35da4c67f078
Tim Staley <git@timstaley.co.uk>
2018-02-07 20:11:56 +0000

    Fix Issue #4 and add configuration for comet-db-directory.

--- examples/listen_for_voevents.sh
+++ examples/listen_for_voevents.sh
@@ -11,20 +11,28 @@
 LOCALIVORN=ivo://fpstoolstest/$(hostname)
 
 # This is the script that comet will pass every VOEvent to, for processing:
-HANDLER=process_voevent_from_stdin_2.py
+HANDLER=./process_voevent_from_stdin_2.py
 
 # The remote broker that we will subscribe to. The '--remote' flag can be
 # repeated with different addresses if you want to  listen to multiple remotes.
-REMOTE=voevent.4pisky.org
+REMOTE=localhost
 
+# Comet maintains a local 'dbm' database directory, where it keeps track of what it's seen before.
+# By default this is /tmp - which is usually fine, but if you're running multiple instances on one 
+# machine then you should separate them, otherwise they'll both read from the same database and you'll
+# get confusing results (e.g. 'I have seen this message before' when it really hasn't).
+EVENTDB_PATH=./cometdbtemp
 
 # Put it all together:
+mkdir -p $EVENTDB_PATH
 /usr/bin/env twistd -n comet \
     --verbose \
     --receive \
     --local-ivo=$LOCALIVORN \
     --remote=$REMOTE \
-    --cmd=$HANDLER
+    --cmd=$HANDLER \
+    --eventdb=$EVENTDB_PATH \
+
 
 
 
